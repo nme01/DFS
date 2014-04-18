fff7cf180e286b5efd5670255085a40b5aec1509
Mateusz Statkiewicz <mat.statkiewicz@gmail.com>
2014-04-18 15:15:33 +0200

    Added database script, initial version for DBManager, 2 enums with mappings for ServerRole and FilesStatus

---
+++ src/main/java/rso/dfs/ServerRole.java
@@ -0,0 +1,41 @@
+package rso.dfs;
+
+/**
+ * @author Mateusz Statkiewicz
+ * 
+ * Enum describing server roles, with mapping to DB signs
+ * */
+
+public enum ServerRole {
+	
+	Master('m'),
+	Shadow('h'),
+	Slave('l');
+	
+	private final char role; 
+	
+	/**
+	 * @param role DB role sign
+	 */
+	ServerRole(char role) {
+		this.role=role;
+	}
+
+	/**
+	 * @return DB role sign
+	 */
+	public char getRoleChar() {
+		return role;
+	};
+	
+	/**
+	 * @param r + DB role sign
+	 * @return corresponding ServerRole
+	 */
+	static public ServerRole getServerRole(char r)
+	{
+		for (ServerRole sr : ServerRole.values())
+			if (sr.getRoleChar() == r) return sr;
+		return null;
+	};
+}
