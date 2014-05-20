1fb2e09a2fee4ce8dba2144a4ed5bcdbe47ba518
Adam Papros <adam.papros@gmail.com>
2014-05-20 14:16:05 +0200

    Clean up.

--- dfs-client/src/main/java/rso/dfs/client/commands/GetCommand.java
+++ dfs-client/src/main/java/rso/dfs/client/commands/GetCommand.java
@@ -1,7 +1,5 @@
 package rso.dfs.client.commands;
 
-import java.io.File;
-
 import rso.dfs.client.handlers.GetHandler;
 
 /**
--- dfs-client/src/main/java/rso/dfs/client/handlers/RemoveHandler.java
+++ dfs-client/src/main/java/rso/dfs/client/handlers/RemoveHandler.java
@@ -1,23 +1,11 @@
 package rso.dfs.client.handlers;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 
-import rso.dfs.commons.DFSConstans;
 import rso.dfs.commons.DFSProperties;
-import rso.dfs.generated.FilePart;
-import rso.dfs.generated.FilePartDescription;
-import rso.dfs.generated.GetFileParams;
 import rso.dfs.generated.Service;
-import rso.dfs.utils.DFSArrayUtils;
 import rso.dfs.utils.DFSTSocket;
-import rso.dfs.utils.IpConverter;
 
 /**
  * @author Adam Papros <adam.papros@gmail.com>
