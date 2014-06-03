eade18a2272418d8835e2fc0093a42ebbdabd254
yakcyll <yakcyll@gmail.com>
2014-06-03 02:28:58 +0200

    WIP: inb4 merge

--- dfs-server/src/main/java/rso/dfs/model/dao/psql/DFSRepositoryImpl.java
+++ dfs-server/src/main/java/rso/dfs/model/dao/psql/DFSRepositoryImpl.java
@@ -149,13 +149,13 @@ public class DFSRepositoryImpl extends Thread implements DFSRepository {
 
 	@Override
 	public List<Server> getSlaves() {
-		log.debug("Fetching slaves.");
+		//log.debug("Fetching slaves.");
 		return masterDAO.fetchServersByRole(ServerRole.SLAVE);
 	}
 
 	@Override
 	public List<Server> getShadows() {
-		log.debug("Fetching shadows.");
+		//log.debug("Fetching shadows.");
 		return masterDAO.fetchServersByRole(ServerRole.SHADOW);
 	}
 
@@ -302,7 +302,7 @@ public class DFSRepositoryImpl extends Thread implements DFSRepository {
 
 	@Override
 	public List<Server> getDownServers() {
-		log.debug("Fetching down servers.");
+		//log.debug("Fetching down servers.");
 		return masterDAO.fetchServersByRole(ServerRole.DOWN);
 	}
 	
